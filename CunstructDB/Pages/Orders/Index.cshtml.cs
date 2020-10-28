ae58a73652b17f6e94c84ad871f4e8229db7813c
Nairaceorex <wwwolf2002@gmail.com>
2020-10-28 13:49:13 +0300

    Update page Orders(all Index file)

--- CunstructDB/Pages/Orders/Index.cshtml.cs
+++ CunstructDB/Pages/Orders/Index.cshtml.cs
@@ -20,10 +20,19 @@ namespace CunstructDB.Pages.Orders
         }
 
         public IList<Order> Order { get;set; }
+        public IList<Customer> Customer { get; set; }
+        public IList<TypeOfJob> TypeOfJob { get; set; }
+        public IList<Brigade> Brigade { get; set; }
+        public IList<Staff> Staff { get; set; }
 
         public async Task OnGetAsync()
         {
             Order = await _context.Order.ToListAsync();
+            Customer = await _context.Customer.ToListAsync();
+            TypeOfJob = await _context.TypeOfJob.ToListAsync();
+            Brigade = await _context.Brigade.ToListAsync();
+            Staff = await _context.Staff.ToListAsync();
+
         }
     }
 }
